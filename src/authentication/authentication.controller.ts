2b2eb81bf29de446208d1aec74145520483e5c77
a.reshetov <a.reshetov@12storeez.com>
2021-09-23 21:35:58 +0700

    create authentication controller

---
+++ src/authentication/authentication.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+
+@Controller('authentication')
+export class AuthenticationController {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  @Post('registration')
+  registration(@Body() userDto: CreateUserDto) {
+    return this.authenticationService.registration(userDto);
+  }
+
+  @Post('/login')
+  login(@Body() userDto: CreateUserDto) {
+    return this.authenticationService.login(userDto);
+  }
+}
