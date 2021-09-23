f922e88b541093d409b60a1f641aa00b3dc8bfb4
a.reshetov <a.reshetov@12storeez.com>
2021-09-23 21:32:17 +0700

    connect DB use typeorm and add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserEntity } from './user/entity/user.entity';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE_NAME,
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UserModule,
+    AuthenticationModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
